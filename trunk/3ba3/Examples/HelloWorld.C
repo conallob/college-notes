b6bab5c54cd4258e38ef30d8810e1ef70c12b469
Conall O'Brien <conall@conall.net>
2004-10-14 20:13:32 +0000

    Added 3BA3 examples
    git-svn-id: file:///home/conall/.svk/local@298 df5d9cd5-2fe6-0310-9432-a9a2fbf40881
    
    committer: conall <conall@df5d9cd5-2fe6-0310-9432-a9a2fbf40881>

---
+++ trunk/3ba3/Examples/HelloWorld.C
@@ -0,0 +1,10 @@
+#include <unistd.h>
+#include <strings.h>
+#include "General.h"
+
+main()
+{
+  char* output_string = "Hello World.\n";
+  if (write( STDOUT_FILENO, output_string, strlen(output_string) ) == -1)
+    syserr("Write failed");
+}
