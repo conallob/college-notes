86aa318ed96cebd021a04e54ce78c338a33722dc
Conall O'Brien <conall@conall.net>
2004-12-07 21:23:23 +0000

    Created 1A project.
    o Recyclying components from 2BA4, with some minor edits.
    
    o Components now ready to be tested include:
      - AND_GATE
      - or_inv_gate
      - addr_counter
      - MEMORY
    
    o Components currently incomplete or not started include:
      - D-LATCH
      - shiftregister
    
    
    git-svn-id: file:///home/conall/.svk/local@407 df5d9cd5-2fe6-0310-9432-a9a2fbf40881
    
    committer: conall <conall@df5d9cd5-2fe6-0310-9432-a9a2fbf40881>

---
+++ trunk/3ba5/Assignments/1A/or_inv_gate.vhdl
@@ -0,0 +1,19 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+--  Uncomment the following lines to use the declarations that are
+--  provided for instantiating Xilinx primitive components.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity or_inv_gate is
+end or_inv_gate;
+
+architecture Behavioral of or_inv_gate is
+
+begin
+
+
+end Behavioral;
