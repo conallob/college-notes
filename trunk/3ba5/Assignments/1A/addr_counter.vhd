19867da4ff141c3372e8db5ed5b71b13671df828
Conall O'Brien <conall@conall.net>
2004-12-09 22:33:59 +0000

    o Attempted addr_counter test bench. Current issue blocking testing is connected to an unknown initial value for the inout bus COUNT. Have attempted to apply RESET to initialise to "00000" with no luck.
    o Copied MEMORY.VHD to MEM.VHD after Xilinx complained about MEMORY.VHD being
    locked by an existing program. Same error occurs using MEM.VHD. :-/
    
    o Created MEM_TB to test memory. Unknown problem currently blocking testing.
    
    o Copied original adr_counter code to adddr_counter_orig.vhdl and copied sample
    counter code from Xilinx library. No improvement.
    
    
    git-svn-id: file:///home/conall/.svk/local@416 df5d9cd5-2fe6-0310-9432-a9a2fbf40881
    
    committer: conall <conall@df5d9cd5-2fe6-0310-9432-a9a2fbf40881>

--- trunk/3ba5/Assignments/1A/addr_counter.vhd
+++ trunk/3ba5/Assignments/1A/addr_counter.vhd
@@ -6,8 +6,8 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 entity addr_counter is
 
 	Port ( 
-		 CLK, RESET: in STD_LOGIC;
-		 COUNT: inout STD_LOGIC_VECTOR(5 downto 0);
+		 CLK, RESET : in STD_LOGIC;
+		 COUNT: out STD_LOGIC_VECTOR(5 downto 0)
 		 );
 	
 end addr_counter;
@@ -15,15 +15,16 @@ end addr_counter;
 architecture Behavioral of addr_counter is
 
 begin
-
-process (CLK, RESET) 
+ 
+process (CLK, RESET)
+ 
 begin
 	if RESET='1' then 
 		COUNT <= "000000";
-		elsif CLK='1' and CLK'event then
-               	COUNT <= COUNT + 1;
-		end if;
-	end if;
+	elsif CLK='1' and CLK'event and RESET='0' then
+		COUNT <= COUNT + 1;
+   	end if;
 end process;
 
 end Behavioral;
+ 
