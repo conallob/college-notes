17a5f5a7de9ee546fee0267042078f4055ec7b52
Conall O'Brien <conall@conall.net>
2004-12-17 12:36:48 +0000

    Decided to be lazy instead, declared parameters as public...
    git-svn-id: file:///home/conall/.svk/local@422 df5d9cd5-2fe6-0310-9432-a9a2fbf40881
    
    committer: conall <conall@df5d9cd5-2fe6-0310-9432-a9a2fbf40881>

--- trunk/3ba3/OS/Assignments/Deadlock/1/Resource.h
+++ trunk/3ba3/OS/Assignments/Deadlock/1/Resource.h
@@ -7,23 +7,11 @@ class Resource {
 					 // {a,b,c}[0] is the max value
 					 //
 					 // {a,b,c}[1-4] are the realtime values
-		  
+		 	
 					 int a[5], b[5], c[5];
-
+					 
 					 // realtime paramaters for sleep()
 		  
 					 int sleeps[4];
 
-					 Resource();
-
 };
-
-Resource::Resource() {
-
-		  int a[5] = {0, 0, 0, 0, 0};
-		  int b[5] = {0, 0, 0, 0, 0};
-		  int c[5] = {0, 0, 0, 0, 0};
-		  
-		  int sleeps[4]  = {0, 0, 0, 0};
-		  
-}
