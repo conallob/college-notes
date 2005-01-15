7e5ef0bea509c8820416a8d61183dc842f1e3df4
Conall O'Brien <conall@conall.net>
2005-01-15 17:26:58 +0000

    Tutorial4.py almost working.
    Current issues to be resolved before submission in Tues 18th include:
    
    o Runtime error when for loop index k or n go out of bounds breaking the loop.
      - Problem appeared when hits and misses counters were introducted.
    
    o Incorrect Hit and Miss results.
      - Review logic of tag checks for a Hit or Miss.
    
    
    git-svn-id: file:///home/conall/.svk/local@462 df5d9cd5-2fe6-0310-9432-a9a2fbf40881
    
    committer: conall <conall@df5d9cd5-2fe6-0310-9432-a9a2fbf40881>

--- trunk/3ba4/I/Tutorials/Tutorial4.py
+++ trunk/3ba4/I/Tutorials/Tutorial4.py
@@ -2,6 +2,8 @@
 
 # $Id$
 
+import math;
+
 # The Tutorial memory address values.
 
 tut_seq = [
@@ -14,27 +16,61 @@ tut_seq = [
 # L, K, N values.
 # Currently hard coded, I'll take user input later...
 
-L = 16;
-K = 1;
-N = 8;
+L = 16; # length in bits
+K = 8;  # 
+N = 1;  # 
 
 # Cache tags and data
 
-cache_tags, cache_data;
+cache_tags = [];
+
+# Initialise cache_tags to all -1s...
+
+for a in range(N):
+	cache_tags.append(-1);
+	cache_tags[a] = [];
+	for b in range(K):
+		cache_tags[a].append(-1);
+		b = b + 1;
+	a = a + 1;
+
+# A simple wrapper function to return an integer from math.log(x, 2)
+
+def ilog(x):
+	return int(math.log(x, 2));
+
+hits, misses = 0, 0;
 
 for i in range(len(tut_seq)):
-	i = i + 1
-	current = tut_seq[i]; # Select the current memory address
+	current = int(tut_seq[i]); # Select the current memory address
 	# Generate line, set and tag info from current
-	line = addr & log(L);
-	set = (current >> log(L)) & log(K);
-	tag = (current >> log(K+L));
+	line = (current & ilog(L));
+	set = ((current >> ilog(L)) & ilog(K)) - 1;
+	tag = (current >> ilog((K+L)));
 	# Now, let's see if we have a relavent cache for current's tag...
-	for n in range(N):
-		n = n + 1
+	n = set; # skip straight to the appropriate set
+	if set <= N:
 		for k in range(K):
-			k = k + 1
-			if cache_tags[n][k] == tag:
-				# Tag found. Woohoo, a Hit!!
-			elif cache_tags[n][k] != tag:
+			print k;
+			if cache_tags[n][k] == -1:
 				# Tag not found, we've got a Miss... :(
+				print "[31mMiss[m";
+				cache_tags[n][k] = tag;
+				print "(",n, k,")", tag, "- Ammended";
+				misses = misses + 1;
+			if cache_tags[n][k] != tag:
+				# Tag not found, we've got a Miss... :(
+				print "[31mMiss[m -", "(",n, k,")", cache_tags[n][k], "- Popped";
+				cache_tags[n].pop();
+				print "(",n, k,")", tag, "- Pushed";
+				cache_tags[n].append(tag);
+				misses = misses + 1;
+			elif cache_tags[n][k] == tag:
+				# Tag found. Woohoo, a Hit!!
+				print "[32mHit[m  -", "(",n, k,")", cache_tags[n][k];
+				hits = hits + 1;
+			k = k + 1;
+		else:
+			pass;	# out of bounds exception
+	i = i + 1;
+print "Hits:", hits, "Misses:", misses;
