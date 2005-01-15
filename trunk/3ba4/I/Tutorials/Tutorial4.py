3b7d69f72f8f18ea05cd7e9ca8866019ae8d37c1
Conall O'Brien <conall@conall.net>
2005-01-15 02:07:49 +0000

    Began building cache simulator.
    Currently L, K, N are hard coded, final version should accept user input.
    
    Using masks, tag, set and line are all generated from the current
    
    3 nested for loops are used to iterate through the 2D cache layout for
    each element from the list of memory addresses
    
    
    git-svn-id: file:///home/conall/.svk/local@461 df5d9cd5-2fe6-0310-9432-a9a2fbf40881
    
    committer: conall <conall@df5d9cd5-2fe6-0310-9432-a9a2fbf40881>

--- trunk/3ba4/I/Tutorials/Tutorial4.py
+++ trunk/3ba4/I/Tutorials/Tutorial4.py
@@ -2,6 +2,8 @@
 
 # $Id$
 
+# The Tutorial memory address values.
+
 tut_seq = [
 0x0000, 0x0004, 0x000c, 0x2200, 0x00d0, 0x00e0, 0x1130, 0x0028,
 0x113c, 0x2204, 0x0010, 0x0020, 0x0004, 0x0040, 0x2208, 0x0008,
@@ -9,20 +11,30 @@ tut_seq = [
 0x00b0, 0x1100, 0x0028, 0x0064, 0x0070, 0x00d0, 0x0008, 0x3394
 ];
 
-q = []
+# L, K, N values.
+# Currently hard coded, I'll take user input later...
 
-i = 0;
+L = 16;
+K = 1;
+N = 8;
 
-print(tut_seq[15]);
+# Cache tags and data
 
-for a in range(len(tut_seq)):
-	i = i+1;
-	if len(q) < 8:
-		q.append(hex(tut_seq[a]));
-		print "Pushed", hex(tut_seq[a]), "[32mOn[37m the queue";
-	else:
-		print "Popped", q[0], "[31mOff[37m the queue";
-		q.pop();
-		q.append(hex(tut_seq[a]));	
-		print "Pushed", q[7], "[32mOn[37m the queue";
-print(q);
+cache_tags, cache_data;
+
+for i in range(len(tut_seq)):
+	i = i + 1
+	current = tut_seq[i]; # Select the current memory address
+	# Generate line, set and tag info from current
+	line = addr & log(L);
+	set = (current >> log(L)) & log(K);
+	tag = (current >> log(K+L));
+	# Now, let's see if we have a relavent cache for current's tag...
+	for n in range(N):
+		n = n + 1
+		for k in range(K):
+			k = k + 1
+			if cache_tags[n][k] == tag:
+				# Tag found. Woohoo, a Hit!!
+			elif cache_tags[n][k] != tag:
+				# Tag not found, we've got a Miss... :(
