91c43efa28263e51c55997bf00d6341488888373
Conall O'Brien <conall@conall.net>
2005-01-21 15:16:44 +0000

    Converted LexicalAnaylser to use a dictionary object for searching.
    Lexical Analyser theory almost complete, system to deal with how to return
    a CHAR/STRING, DIGIT token of length x or ":=" token still to be done.
    
    
    git-svn-id: file:///home/conall/.svk/local@486 df5d9cd5-2fe6-0310-9432-a9a2fbf40881
    
    committer: conall <conall@df5d9cd5-2fe6-0310-9432-a9a2fbf40881>

--- trunk/3ba7/I/Assignments/ducare.py
+++ trunk/3ba7/I/Assignments/ducare.py
@@ -21,54 +21,65 @@ def LexicalAnalyser(input):
 
 	# A function for lexical analysis using a FSM
 	
-#	alphabet = (r'[A-Z][a-z]', r'[0-9]', 'DIV', 'MOD', 'WRITE', 'END', ':', '=', 
-#					 ',', '+', '-', '*', '|', '(', ')', r'(\(n|r|t)| )', '\004');
-
-	valid_transitions = [
-	('CHAR',  'CHAR'),	# r"([A-Z]|[a-z])"
-	('CHAR',  'ACCEPT'),
-	('DIGIT', 'DIGIT'),  # r"\d"
-	('DIGIT', 'ACCEPT'), 
-	('DIV',   'ACCEPT'),
-	('MOD',   'ACCEPT'),
-	('WRITE', 'ACCEPT'),
-	('END',   'ACCEPT'),
-	(':',     '='),
-	('=',     'ACCEPT'),
-	(',',     'ACCEPT'),
-	('+',     'ACCEPT'),
-	('-',     'ACCEPT'),
-	('*',     'ACCEPT'),
-	('|',     'ACCEPT'),
-	('(',     'ACCEPT'),
-	('(',     'ACCEPT'),
-	(')',     'ACCEPT'),
-	('WHITESPACE', 'ACCEPT'), # r"\s"
-	('ENDMARKER',  'ACCEPT'),
-	];
+	valid_transitions = {
+	'CHAR'   		:	'ACCEPT',
+	'DIGIT'  		:	'ACCEPT', 
+	'DIV'    		:	'ACCEPT',
+	'MOD'    		:	'ACCEPT',
+	'WRITE'  		:	'ACCEPT',
+	'END'    		:	'ACCEPT',
+	':'        		:	'=',
+	'='        		:	'ACCEPT',
+	','        		:	'ACCEPT',
+	'+'        		:	'ACCEPT',
+	'-'        		:	'ACCEPT',
+	'*'        		:	'ACCEPT',
+	'|'        		:	'ACCEPT',
+	'('        		:	'ACCEPT',
+	'('        		:	'ACCEPT',
+	')'        		:	'ACCEPT',
+	'WHITESPACE' 	:	'ACCEPT',	# r"\s"
+	'ENDMARKER'		:	'ACCEPT',
+	};
 
 	output = [];
+	n = 0;
 
 	for a in range(len(input)):
-		for b in range(len(valid_transitions)):
-			if valid_transitions[b][0] = "CHAR":
-				if re.match(r"([A-Z]|[a-z])", input[a]:
-					output.append(input[a]); # Single char check only...
-			if valid_transitions[b][0] = "DIGIT":
-				if re.match(r"\d", input[a]:
-					output.append(input[a]); # Single digit check only...
-			if valid_transitions[b][0] = "WHITESPACE":	
-				if re.match(r"\s", input[a]:
-			if valid_transitions[b][0] = "ENDMARKER":
-				if input[a] == '\004':
-				# ACCEPT
-			elif input[a] == 
-					
+		if re.match(r"[A-Za-z]", input[a]):
+			check = 'CHAR';
+		elif re.match(r"\d", input[a]):
+			check = 'DIGIT';
+		elif re.match(r"\s", input[a]):
+			check = 'WHITESPACE';
+		elif (input[a] == '\004'):
+			check = 'ENDMARKER';
+		else:
+			check = input[a];
+		# a + 1
+		if re.match(r"[A-Za-z]", input[a + 1]):
+			nextcheck = 'CHAR';
+		elif re.match(r"\d", input[a + 1]):
+			nextcheck = 'DIGIT';
+		elif re.match(r"\s", input[a + 1]):
+			nextcheck = 'WHITESPACE';
+		elif (input[a + 1] == '\004'):
+			nextcheck = 'ENDMARKER';
+		else:
+			nextcheck = input[a + 1];
+		#
+		if valid_transitions.has_key(check):
+			if valid_transitions[check] == "ACCEPT":
+				output.append(input(a));
+			#elif valid_transitions[check] == nextcheck:
+			#	n = 1;
+	return output;					
 				
 
 # Main function
 
 if '__main__' == __name__:
+	import re
 	from optparse import OptionParser
 	parser = OptionParser();
 	parser.add_option("-i", "--input", dest="inputfile", 
@@ -76,4 +87,4 @@ if '__main__' == __name__:
 	(options, args) = parser.parse_args();
 	inputfile = open(options.inputfile, 'r');
 	inputfile = inputfile.read();
-	lexicalanalyser();
+	LexicalAnalyser(inputfile);
