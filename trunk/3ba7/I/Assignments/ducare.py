e3cccf4e6b11a88a56ca2f0a626f192f55b991cd
Conall O'Brien <conall@conall.net>
2005-01-21 16:57:52 +0000

    Got LexicalAnalyser parsing input and returning tokens.
    However, approach to dealing with strings needs to be reviewed,
    currently STRINGS and reserved words are being treated as CHARs.
    
    
    git-svn-id: file:///home/conall/.svk/local@487 df5d9cd5-2fe6-0310-9432-a9a2fbf40881
    
    committer: conall <conall@df5d9cd5-2fe6-0310-9432-a9a2fbf40881>

--- trunk/3ba7/I/Assignments/ducare.py
+++ trunk/3ba7/I/Assignments/ducare.py
@@ -13,10 +13,6 @@
 # Functions
 #
 
-
-# Setup command line parameters
-
-
 def LexicalAnalyser(input):
 
 	# A function for lexical analysis using a FSM
@@ -38,53 +34,76 @@ def LexicalAnalyser(input):
 	'('        		:	'ACCEPT',
 	'('        		:	'ACCEPT',
 	')'        		:	'ACCEPT',
-	'WHITESPACE' 	:	'ACCEPT',	# r"\s"
+	'WHITESPACE' 	:	'ACCEPT',
 	'ENDMARKER'		:	'ACCEPT',
 	};
 
 	output = [];
 	n = 0;
 
-	for a in range(len(input)):
-		if re.match(r"[A-Za-z]", input[a]):
+	for i in range(len(input)):
+		if re.match(r"[A-Za-z]", input[i]):
 			check = 'CHAR';
-		elif re.match(r"\d", input[a]):
+		elif re.match(r"\d", input[i]):
 			check = 'DIGIT';
-		elif re.match(r"\s", input[a]):
+		elif re.match(r"\s", input[i]):
 			check = 'WHITESPACE';
-		elif (input[a] == '\004'):
+		elif (input[i] == '\004'):
 			check = 'ENDMARKER';
 		else:
-			check = input[a];
-		# a + 1
-		if re.match(r"[A-Za-z]", input[a + 1]):
-			nextcheck = 'CHAR';
-		elif re.match(r"\d", input[a + 1]):
-			nextcheck = 'DIGIT';
-		elif re.match(r"\s", input[a + 1]):
-			nextcheck = 'WHITESPACE';
-		elif (input[a + 1] == '\004'):
-			nextcheck = 'ENDMARKER';
-		else:
-			nextcheck = input[a + 1];
-		#
-		if valid_transitions.has_key(check):
-			if valid_transitions[check] == "ACCEPT":
-				output.append(input(a));
-			#elif valid_transitions[check] == nextcheck:
-			#	n = 1;
+			check = input[i];
+		if i < (len(input) - 1):
+			# i + 1
+			if re.match(r"[A-Za-z]", input[i + 1]):
+				nextcheck = 'CHAR';
+			elif re.match(r"\d", input[i + 1]):
+				nextcheck = 'DIGIT';
+			elif re.match(r"\s", input[i + 1]):
+				nextcheck = 'WHITESPACE';
+			elif (input[i + 1] == '\004'):
+				nextcheck = 'ENDMARKER';
+			else:
+				nextcheck = input[i + 1];
+		# Now to gather the tokens we're going to return
+		if n == 0:
+			if valid_transitions.has_key(check):
+				if valid_transitions[check] == "ACCEPT":
+					output.append(input[i]);
+					n = 0;  # Clear n
+				elif valid_transitions[check] == nextcheck:
+					n += 1;
+			#else: # Error checking
+				#sys.stderr.write("Error at symbol", input[a]);
+		elif n > 0:
+			if valid_transitions.has_key(check):
+				if valid_transitions[check] == "ACCEPT":
+					output.append(input[(i - n):i]);
+					n = 0;  # Clear n
+				elif valid_transitions[check] == nextcheck:
+					n += 1;
+			#else: # Error checking
+				#sys.stderr.write("Error at symbol", input[a]);
 	return output;					
 				
 
 # Main function
 
 if '__main__' == __name__:
-	import re
+	import re, sys
+	# Setup command line parameters
 	from optparse import OptionParser
 	parser = OptionParser();
 	parser.add_option("-i", "--input", dest="inputfile", 
 							help="Read input from FILE", metavar="FILE");
+	parser.add_option("-e", "--inline", dest="cmdinput", 
+							help="Read input from command line paramater", metavar="STRING");
 	(options, args) = parser.parse_args();
+	# Read the input File
 	inputfile = open(options.inputfile, 'r');
 	inputfile = inputfile.read();
-	LexicalAnalyser(inputfile);
+	tokens = LexicalAnalyser(inputfile);
+	for c in range(len(tokens)):
+		if tokens[c] == "\n":
+			print "\n";
+		else:
+			print tokens[c];
