252e1c6028e4d61f98bae2b16f855c8c03fc3089
Conall O'Brien <conall@conall.net>
2005-01-22 23:29:56 +0000

    Now use splicing of $LastChangedRevision$ string to return just the last modified rev number when ducare.py --version is called
    git-svn-id: file:///home/conall/.svk/local@496 df5d9cd5-2fe6-0310-9432-a9a2fbf40881
    
    committer: conall <conall@df5d9cd5-2fe6-0310-9432-a9a2fbf40881>

--- trunk/3ba7/I/Assignments/ducare.py
+++ trunk/3ba7/I/Assignments/ducare.py
@@ -8,6 +8,8 @@
 # Variables
 #
 
+revision = "$LastChangedRevision$";
+rev = revision.split(' ')[1][:-1];
 
 #
 # Functions
@@ -97,7 +99,7 @@ if '__main__' == __name__:
 	import re, sys
 	# Setup command line parameters
 	from optparse import OptionParser
-	parser = OptionParser(usage="%prog [-f | --file] | [-i | --input] | --version", version="%prog $LastChangedRevision$");
+	parser = OptionParser(usage="%prog [-f | --file] | [-i | --input] | --version", version="%prog ".join(rev));
 	parser.add_option("-f", "--file", dest="inputfile", help="Read input from FILE", metavar="FILE");
 	parser.add_option("-i", "--input", dest="cmdinput", help="Read input from STRING", metavar="STRING");
 	(options, args) = parser.parse_args();
