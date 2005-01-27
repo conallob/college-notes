38c0d49d76224dabdd9dd73d17561487b91b7a09
Conall O'Brien <conall@conall.net>
2005-01-27 18:25:05 +0000

    Initial check in.
    Barrel Shifter working and testbanch screenshot saved.
    
    
    git-svn-id: file:///home/conall/.svk/local@505 df5d9cd5-2fe6-0310-9432-a9a2fbf40881
    
    committer: conall <conall@df5d9cd5-2fe6-0310-9432-a9a2fbf40881>

---
+++ trunk/3ba5/Assignments/1b/addr_counter.vhd
@@ -0,0 +1,30 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity addr_counter is
+
+	Port ( 
+		 CLK, RESET : in STD_LOGIC;
+		 COUNT: out STD_LOGIC_VECTOR(5 downto 0)
+		 );
+	
+end addr_counter;
+
+architecture Behavioral of addr_counter is
+
+begin
+ 
+process (CLK, RESET)
+ 
+begin
+	if RESET='1' then 
+		COUNT <= "000000";
+	elsif CLK='1' and CLK'event and RESET='0' then
+		COUNT <= COUNT + 1;
+   	end if;
+end process;
+
+end Behavioral;
+ 
