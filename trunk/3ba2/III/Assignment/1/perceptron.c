880e2e4f9ee705c032bee0081f199a51cee65e39
Conall O'Brien <conall@conall.net>
2005-03-17 18:05:56 +0000

    Created compute_perception_outputs function.
    Corrected initialize_perceptron_weights to populate a 2D [j][i]
    array instead of a 1D [i*j] array
    
    
    Scrapped previous plan for update_perceptron_network and
    rewrote it to be used to update perception biases, weights
    and absolute_change aka Stage 5.
    
    
    All that's left is to get a few nagging questions answered and
    attempt to test.
    
    
    git-svn-id: file:///home/conall/.svk/local@554 df5d9cd5-2fe6-0310-9432-a9a2fbf40881
    
    committer: conall <conall@df5d9cd5-2fe6-0310-9432-a9a2fbf40881>

--- trunk/3ba2/III/Assignment/1/perceptron.c
+++ trunk/3ba2/III/Assignment/1/perceptron.c
@@ -62,7 +62,7 @@ void initialize_perceptron_biases(struct perceptron_network *network, unsigned i
 {
    int i;
 
-   for (i = 0; i < network->num_outputs; i++)
+   for (i = 1; i < network->num_outputs; i++)
    {
        network->biases[i] = (drand48() - 0.5) * range;
    }
@@ -71,29 +71,39 @@ void initialize_perceptron_biases(struct perceptron_network *network, unsigned i
 
 void initialize_perceptron_weights(struct perceptron_network *network, unsigned int range)
 {
-    int i;
-
-    for (i = 0; i < network->num_inputs * network->num_outputs; i++)
+    int i, j;
+	 
+    for (j = 1; i < network->num_outputs; j++)
     {
-        network->weights[i] = (drand48() - 0.5) * range;
+    	  for (i = 1; i < network->num_inputs; i++)
+		  {
+        		network->weights[j][i] = (drand48() - 0.5) * range;
+		  }
     }
 }
 
 
-bool update_perceptron_network(struct perceptron_network *network, int *input, unsigned int num_inputs)
+bool update_perceptron_network(struct perceptron_network *network, int *input, unsigned int num_inputs, int *training_data_t)
 {
     if (num_inputs > network->num_inputs)
     {
         return(false);
     }
 	
-	 int i;
+	 int i, j;
 
-	 for (i = 0; i < network->num_outputs; i++)
+	 for (j = 1; j < network->num_outputs; j++)
 	 {
-	 	  
+	 	  for (i = 1; i < network->num_inputs; i++)
+		  {
+				if(network->output[j] != training_data_t[j])
+				{
+					 network->biases[j] += training_data_t[j];
+					 network->weights[j][i] += /* TODO - Pester someone who knows what t_j x_i means*/ ;
+					 network->absolute_change += /* t_j x_i */;
+				}
+		  }
 	 }
-    /* return(true); */
 }
 
 
@@ -112,26 +122,69 @@ void set_activation_inputs(struct perception_network *network, int *training_dat
 
 	for (i = 0; i < network->num_inputs; i++)
 	{
-		network->input[i] = training_data_s[1];
+		network->input[i] = training_data_s[i];
 	}
 	
 }
+
+void compute_perception_outputs(struct perception_network *network, unsigned double theta)
+{
+	int i, j;
+	int *y_in
+
+	for(j = 1; j < network->num_outputs; j++)
+	{
+			y_in = network->biases[j];
+
+			for(i = 1; i < network->num_inputs; i++)
+			{
+					y_in[j] += network->input[i] + network->weights[j][i];
+			}
+			
+			if (y_in[y] > theta)
+			{
+					  
+					  network->output[j] = 1;
+					  
+			} else if (y_in[j] >= (-1 * theta) && y_in <= theta)
+			{
+					  
+					  network->output[j] = 0;
+					  
+			} else if (y_in[j] < (-1 * theta))
+			{
+					  network->output[j] = -1;
+			}
+ 			
+	}
+	
+}
+
 
 int main(int argc, char **argv)
 {
 
     struct perceptron_network *network;
 	 int *s *t;
+	 double theta;
 	 FILE ifp;
 
-	 /*  
-	  * Step 0: And on the first day...
-	  */
-	 
+	 /* Step 0 */
     network = create_perceptron_network(64, 8);
 
     initialize_perceptron_biases(network, 1);
     initialize_perceptron_weights(network, 1);
+
+	 if (argv[2] != NULL)
+	 {
+				
+			theta = argv[2];
+			
+	 } else {
+			
+			theta = 1;
+
+	 }
 
 	 /* Parse training data for s and t from data file... */
 	
@@ -148,7 +201,13 @@ int main(int argc, char **argv)
 	 /* Step 1*/
     do {
 			/* Step 2 */
-        update_perceptron_network(network, NULL, 0);
+			/* Step 3 */
+		   set_activation_inputs(network, s);
+			/* Step 4 */
+			compute_perception_outputs(network, theta);	
+			/* Step 5 */
+         update_perceptron_network(network, NULL, 0);
+		  
     } while (network->absolute_change > 0.0001);
 
 	 
