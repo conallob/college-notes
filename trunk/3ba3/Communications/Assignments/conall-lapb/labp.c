ae3b74e1433b82278c97973c73e3d0d34f81fb07
Conall O'Brien <conall@conall.net>
2005-04-11 19:02:04 +0000

    * Copied p3.c to lapb.c
    git-svn-id: file:///home/conall/.svk/local@578 df5d9cd5-2fe6-0310-9432-a9a2fbf40881
    
    committer: conall <conall@df5d9cd5-2fe6-0310-9432-a9a2fbf40881>

---
+++ trunk/3ba3/Communications/Assignments/conall-lapb/labp.c
@@ -0,0 +1,82 @@
+/* Protocol 7 (labp) allows .
+ *
+ * To compile: cc -o protocol7 labp.c simulator.o
+ * To run: protocol7 events timeout  pct_loss  pct_cksum  debug_flags
+ *
+ * Written by Conall O'Brien
+
+
+
+#define MAX_SEQ 1	/* must be 1 for protocol 3 */
+#define MAX_PROTOCOL 7          /* highest protocol being simulated */
+                                /* THIS SHOULD BE REMOVED */
+typedef enum  {frame_arrival, cksum_err, timeout} event_type;
+#include "protocol.h"
+
+void sender_lapb(void);
+void receiver_lapb(void);
+
+int main(int argc, char *argv[])
+{
+  int timeout_interval, pkt_loss, garbled, debug_flags;
+  long event;
+
+  if (!parse_first_five_parameters(argc, argv, &event, &timeout_interval,
+                                   &pkt_loss, &garbled, &debug_flags))  {
+    printf ("Usage: lapb events timeout loss cksum debug\n");
+    exit(1);
+  }
+
+  printf("\n\n Simulating LABP\n");
+  start_simulator(sender_lapd, receiver_lapb, event, timeout_interval, pkt_loss, garbled, debug_flags);
+}
+
+void sender_lapb(void)
+{
+  seq_nr next_frame_to_send;	/* seq number of next outgoing frame */
+  frame s;		/* scratch variable */
+  packet buffer;	/* buffer for an outbound packet */
+  event_type event;
+
+  next_frame_to_send = 0;	/* initialize outbound sequence numbers */
+  from_network_layer(&buffer);	/* fetch first packet */
+  while (true) {
+        init_frame_lapb(&s);
+        s.info = buffer;	/* construct a frame for transmission */
+        s.seq = next_frame_to_send;	/* insert sequence number in frame */
+        to_physical_layer_lapb(&s);	/* send it on its way */
+        start_timer(s.seq);	/* if answer takes too long, time out */
+        wait_for_event_lapb(&event);	/* frame_arrival, cksum_err, timeout */
+        if (event == frame_arrival) {
+                from_physical_layer_lapb(&s);	/* get the acknowledgement */
+                if (s.ack == next_frame_to_send) {
+                        from_network_layer_lapb(&buffer);	/* get the next one to send */
+                        inc(next_frame_to_send);	/* invert next_frame_to_send */
+                }
+        }
+  }
+}
+
+void receiver_lapb(void)
+{
+  seq_nr frame_expected;
+  frame r, s;
+  event_type event;
+
+  frame_expected = 0;
+  while (true) {
+        wait_for_event_lapb(&event);	/* possibilities: frame_arrival, cksum_err */
+        if (event == frame_arrival) {
+                /* A valid frame has arrived. */
+                from_physical_layer_lapb(&r);	/* go get the newly arrived frame */
+                if (r.seq == frame_expected) {
+                        /* This is what we have been waiting for. */
+                        to_network_layer_lapb(&r.info);	/* pass the data to the network layer */
+                        inc(frame_expected);	/* next time expect the other sequence nr */
+                }
+                init_frame(&s);
+                s.ack = 1 - frame_expected;	/* tell which frame is being acked */
+                to_physical_layer_lapb(&s);	/* only the ack field is use */
+        }
+  }
+}
