4ab603bef600b9179840c8f421047ba26903039d
Conall O'Brien <conall@conall.net>
2005-04-12 16:05:01 +0000

    * lapb.c:
    	- Added handshake to event_type enum and changed if statement check for event
    	  to a switch statement.
    
    	- Created repeat_algorithm enum to assist in selecting if ARQ or SSR is used
    	  for repeated transmission
    
    	- Added #define definitions of UA, ARQ and SSR control sequences. Perhaps they
    	  should go into simulator.(h|c)
    
    	  int lapb_control_field(lapb_frame *f) needs to be written in simulator.c. I'll do
    	  it after the pub(s) this evening... :)
    
    	- Wrote basic handshake steps in sender_lapb(). Complementary steps for
    	  receiver_lapb() still need to be implemented, along with error checking.
    
    
    git-svn-id: file:///home/conall/.svk/local@581 df5d9cd5-2fe6-0310-9432-a9a2fbf40881
    
    committer: conall <conall@df5d9cd5-2fe6-0310-9432-a9a2fbf40881>

--- trunk/3ba3/Communications/Assignments/conall-lapb/labp.c
+++ trunk/3ba3/Communications/Assignments/conall-lapb/labp.c
@@ -1,4 +1,4 @@
-/* Protocol 7 (labp) allows .
+/* Protocol 7 (labp) simulates...
  *
  * To compile: cc -o protocol7 labp.c simulator.o
  * To run: protocol7 events timeout  pct_loss  pct_cksum  debug_flags
@@ -6,15 +6,27 @@
  * Written by Conall O'Brien
 
 
-
 #define MAX_SEQ 1	/* must be 1 for protocol 3 */
 #define MAX_PROTOCOL 7          /* highest protocol being simulated */
                                 /* THIS SHOULD BE REMOVED */
-typedef enum  {frame_arrival, cksum_err, timeout} event_type;
+
+#define LAPB_UA 0xC6
+#define LAPB_ARQ 0xD0
+#define LAPB_SSR 0xD1
+
+typedef enum  {handshake, frame_arrival, cksum_err, timeout} event_type;
+
+typedef enum {arq, ssr} repeat_algorithm;
+
 #include "protocol.h"
 
 void sender_lapb(void);
 void receiver_lapb(void);
+
+repeat_algorithm method; /* How are we going to handle repeats? */ 
+
+/* method = arq; */
+/* method = ssr; */
 
 int main(int argc, char *argv[])
 {
@@ -37,29 +49,61 @@ void sender_lapb(void)
   lapb_frame s;		/* scratch variable */
   packet buffer;	/* buffer for an outbound packet */
   event_type event;
-  boolean handshake; /* true if handshake sucessful, false otherwise */
 
   next_frame_to_send = 0;	/* initialize outbound sequence numbers */
-  handshake = false; /* initialise handshake flag */
   from_network_layer(&buffer);	/* fetch first packet */
+  event = 
   while (true) {
         init_frame_lapb(&s);
         s.info = buffer;	/* construct a frame for transmission */
- /*       s.seq = next_frame_to_send;	/* insert sequence number in frame */ 
+		  increment_lapb_ns(&s);
         to_physical_layer_lapb(&s);	/* send it on its way */
 		  increment_lapb_ns();
         start_timer(get_lapb_ns(&s));	/* if answer takes too long, time out */
         wait_for_event_lapb(&event);	/* frame_arrival, cksum_err, timeout */
-        if (event == frame_arrival) {
-                from_physical_layer_lapb(&s);	/* get the acknowledgement */
-                if (get_lapb_nr(&s) == next_frame_to_send) {
-                        from_network_layer_lapb(&buffer);	/* get the next one to send */
-                        inc(next_frame_to_send);	/* invert next_frame_to_send */
-								increment_lapb_ns(&s);
-                }
+        switch(event) {
+
+					 /* Handshake mode */
+
+					 case handshake:
+
+								/* Choose Method */
+                        if (method == arq) {
+										  /* We're using Set Automatic Repeat reQuest */
+                         		 s.control = LAPB_ARQ;
+                        } else if(method == ssr) {
+										  /* We're using Set Selective Repeat */
+                         		 s.control = LAPB_SSR;
+                        }
+								to_physical_layer_lapb(&s); /* Send the first frame */
+					 			from_physical_layer_lapb(&s);	/* Wait for the acknowledgement */
+                			if (lapb_control_field(&s) == LAPB_UA) {
+										  event = frame_arrival; /* handshake successful, moving on... */
+								}
+                        	from_network_layer_lapb(&buffer);	/* get the next one to send */
+                        	inc(next_frame_to_send);	/* invert next_frame_to_send */
+									increment_lapb_ns(&s);
+                			}
+
+					 break;
+
+					 /* Arrival Mode */
+
+					 case frame_arrival:
+					 
+					 			from_physical_layer_lapb(&s);	/* get the acknowledgement */
+                			if (get_lapb_nr(&s) == next_frame_to_send) {
+                        	from_network_layer_lapb(&buffer);	/* get the next one to send */
+                        	inc(next_frame_to_send);	/* invert next_frame_to_send */
+									increment_lapb_ns(&s);
+                			}
+							
+					 break;
+					 
         }
   }
 }
+
 
 void receiver_lapb(void)
 {
