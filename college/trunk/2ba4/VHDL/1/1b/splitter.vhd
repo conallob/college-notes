2095d9d9cf227839b53e9264ccfc462fefb9fcce
Conall O'Brien <conall@conall.net>
2006-01-16 02:47:11 +0000

    Removed //college/college
    git-svn-id: file:///home/conall/.svk/local@1144 df5d9cd5-2fe6-0310-9432-a9a2fbf40881
    
    committer: conall <conall@df5d9cd5-2fe6-0310-9432-a9a2fbf40881>

--- college/trunk/2ba4/VHDL/1/1b/splitter.vhd
+++
@@ -1,3 +0,0 @@
--- $Id: splitter.vhd,v 1.3 2004/04/14 21:52:35 conall Exp $
-
-
--- college/trunk/3ba5/Assignments/3/samples/reilly/make/mpi.mk
+++
@@ -1,5 +0,0 @@
-####sk  We use gcc locally so ...
-####sk  CC = mpicc -mpiarch $(ARCH) $(FLAGS)
-CC = mpicc -mpicc gcc -mpiarch $(ARCH) $(FLAGS)
-FC = mpif77 -mpiarch $(ARCH) $(FLAGS)
-COMMS = mpi
