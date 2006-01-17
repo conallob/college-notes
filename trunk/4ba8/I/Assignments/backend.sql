6f2b905ca2b4dbd1130ea9a68a50be13a5cc6f3e
Conall O'Brien <conall@conall.net>
2006-01-17 17:04:28 +0000

    r1446@thog:  conall | 2006-01-17 17:04:10 +0000  Committed initial MySQL table structure for the backend
    git-svn-id: file:///home/conall/.svk/local@1207 df5d9cd5-2fe6-0310-9432-a9a2fbf40881
    
    committer: conall <conall@df5d9cd5-2fe6-0310-9432-a9a2fbf40881>

---
+++ trunk/4ba8/I/Assignments/backend.sql
@@ -0,0 +1,32 @@
+# $Id$
+
+service ENUM ('Hotel', 'Car', 'Bike', 'Airline', 'Train');
+
+create table service (
+	ServiceID	INTEGER	NOT NULL AUTO_INCREMENT,
+	ServiceName	VARCHAR(80) NOT NULL,
+	ServiceType	service NOT NULL,
+	PRIMARY 		KEY(ServiceID)
+);
+
+create table instance (
+	UniqueID		INTEGER NOT NULL AUTO_INCREMENT,
+	ServiceID	INTEGER	NOT NULL AUTO_INCREMENT,
+	ServiceDate	DATE	NOT NULL,
+	ServiceDate	NOT NULL,
+	Capacity		INTEGER	NOT NULL,
+	Cost			INTEGER NOT NULL,
+	PRIMARY 		KEY(UniqueID),
+	FOREIGN 		KEY(ServiceID) REFERENCES service (ServiceID);
+);
+
+create table booking (
+	BookingID	INTEGER NOT NULL AUTO_INCREMENT,
+	UniqueID		INTEGER NOT NULL AUTO_INCREMENT,
+	Booked		BOOLEAN,
+	Flexible		BOOLEAN,
+	Cancelled	BOOLEAN,
+	Confirmed	BOOLEAN,
+	PRIMARY KEY(BookingID),
+	FOREIGN KEY(UniqueID) REFERENCES instance (UniqueID);
+);
