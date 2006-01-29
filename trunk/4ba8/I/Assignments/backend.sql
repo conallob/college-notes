b92d0a3bf3fde3f2727b01c8ebdc5ec1708be1f1
Conall O'Brien <conall@conall.net>
2006-01-29 20:27:54 +0000

    r2368@thog:  conall | 2006-01-25 21:36:28 +0000
    git-svn-id: file:///home/conall/.svk/local@1963 df5d9cd5-2fe6-0310-9432-a9a2fbf40881
    
    committer: conall <conall@df5d9cd5-2fe6-0310-9432-a9a2fbf40881>

--- trunk/4ba8/I/Assignments/backend.sql
+++ trunk/4ba8/I/Assignments/backend.sql
@@ -5,7 +5,7 @@ create table servicetype (
 	TypeID	int(5) NOT NULL auto_increment,
 	TypeName varchar(80) default '',
 	PRIMARY KEY(TypeID)
-);
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 create table servicetype (
 	TypeID	INTEGER NOT NULL AUTO_INCREMENT,
@@ -20,7 +20,7 @@ create table service (
 	Opening		datetime default NULL,
 	Closing		datetime default NULL,
 	PRIMARY KEY(ServiceID)
-);
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 drop table if exists instance;
 create table instance (
@@ -29,8 +29,9 @@ create table instance (
 	ServiceDate	datetime	NOT NULL default '0000-00-00 00:00:00',
 	Capacity		int(5) NOT NULL default 0,
 	Cost			int(5) NOT NULL default 0,
-	PRIMARY 		KEY(UniqueID)
-);
+	PRIMARY 		KEY(UniqueID),
+	FOREIGN 		KEY(ServiceID) REFERENCES service (ServiceID);
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 drop table if exists booking;
 create table booking (
@@ -40,5 +41,6 @@ create table booking (
 	Flexible		int(1),
 	Cancelled	int(1),
 	Confirmed	int(1),
-	PRIMARY KEY(BookingID)
-);
+	PRIMARY KEY(BookingID),
+	FOREIGN KEY(UniqueID) REFERENCES instance (UniqueID);
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
