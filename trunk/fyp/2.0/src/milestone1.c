7c4d1fff940dedf188207d7dc928c7ccabd012bd
Conall O'Brien <conall@conall.net>
2006-02-05 23:12:57 +0000

    r2086@thog:  conall | 2006-02-05 23:10:11 +0000  Got basic functionality working.
     Input is received from stdin (either from local echo, or via a pipe),
     buffered into char* storage, then printed to stdout.
    
    
     To Do:
    
     	* Use a while loop to allow for multi-lined input
     	* Convert storage to an APR linked list, to buffer multi-lined input
    
    
    git-svn-id: file:///home/conall/.svk/local@2205 df5d9cd5-2fe6-0310-9432-a9a2fbf40881
    
    committer: conall <conall@df5d9cd5-2fe6-0310-9432-a9a2fbf40881>

--- trunk/fyp/2.0/src/milestone1.c
+++ trunk/fyp/2.0/src/milestone1.c
@@ -2,21 +2,38 @@
 
 #include "apr.h"
 #include "apr_file_io.h"
+
+#include <string.h>
 	
-int main(int argc, char *argv[])
-{
-		  apr_pool_t *p;
-		  apr_file_t *fp;
+int main(int argc, char *argv[]) {
+
+		  char *buffer;
+		  char* storage;
+
+		  int i = 0;
+
+		  apr_pool_t *p, *q;
+		  apr_file_t *fp_in, *fp_out;
 
 		  apr_initialize();
 
 		  atexit(apr_terminate);
+
 		  apr_pool_create(&p, NULL);
+		  apr_pool_create(&q, NULL);
 
-		  apr_file_open_stdout(&fp, p);
+		  apr_file_open_stdin(&fp_in, p);
+		  apr_file_open_stdout(&fp_out, q);
 
-		  apr_file_printf(fp, argv[1]);
-		  apr_file_printf(fp, "\n");
+		  apr_file_printf(fp_out, "Input:\n");
+
+		  apr_file_gets(buffer, 80, fp_in);
+		  strcpy(buffer, storage);
+		 
+		  apr_file_printf(fp_out, "Output:\n");
+
+		  apr_file_printf(fp_out, storage);
 
 		  return 0;
+
 }
