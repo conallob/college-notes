af8c2af57606335d73844b6da43e36f69431618e
Conall O'Brien <conall@conall.net>
2006-02-08 02:46:33 +0000

    r2138@thog:  conall | 2006-02-07 21:25:22 +0000  * Added proper comments
     * Adjusted debug output to go to stderr, not stdout
    
    
    git-svn-id: file:///home/conall/.svk/local@2229 df5d9cd5-2fe6-0310-9432-a9a2fbf40881
    
    committer: conall <conall@df5d9cd5-2fe6-0310-9432-a9a2fbf40881>

--- trunk/fyp/2.0/src/milestone1.c
+++ trunk/fyp/2.0/src/milestone1.c
@@ -1,39 +1,57 @@
-// $Id$
+/* 
+ * File: milestone1.c 
+ * Description: Milestone #1 - Simple Local Echo Test
+ * Author: Conall O'Brien <conallob@maths.tcd.ie>
+ * Date: $Date$
+ * 
+ * Id: $Id$
+ */
 
+/* Use APR */
 #include "apr.h"
 #include "apr_file_io.h"
 
+/* Use String manipulation functions */
 #include <string.h>
-	
+
+
 int main(int argc, char *argv[]) {
 
-		  char *buffer;
-		  char* storage;
+		  /* input buffer */
+		  char *buffer;  
 
-		  int i = 0;
+		  int i = 0; /* loop counter */
 
-		  apr_pool_t *p, *q;
-		  apr_file_t *fp_in, *fp_out;
+		  /* APR pools, 1 for stdin, stdout & stderr */
+		  apr_pool_t *p, *q, *e; 
+		  /* APR files, for stdin, stdout & stderr */
+		  apr_file_t *fp_in, *fp_out, *fp_err; 
 
-		  apr_initialize();
+		  apr_initialize(); /* Initialise APR */
 
 		  atexit(apr_terminate);
 
-		  apr_pool_create(&p, NULL);
-		  apr_pool_create(&q, NULL);
+		  apr_pool_create(&e, NULL); /* initialise APR pool e */
+		  apr_pool_create(&p, NULL); /* initialise APR pool p */
+		  apr_pool_create(&q, NULL); /* initialise APR pool q */
 
-		  apr_file_open_stdin(&fp_in, p);
-		  apr_file_open_stdout(&fp_out, q);
+		  /* Open stdin, stdout & stderr */
+		  apr_file_open_stderr(&fp_err, e); 
+		  apr_file_open_stdin(&fp_in, p);   
+		  apr_file_open_stdout(&fp_out, q); 
 
-		  apr_file_printf(fp_out, "Input:\n");
+		  /* Debug output */
+		  apr_file_printf(fp_err, "Input:\n"); 
 
-		  apr_file_gets(buffer, 80, fp_in);
-		  strcpy(buffer, storage);
-		 
-		  apr_file_printf(fp_out, "Output:\n");
+		  /* Grab 80 characters from stdin and store in buffer */
+		  apr_file_gets(buffer, 80, fp_in); 
 
-		  apr_file_printf(fp_out, storage);
+		  /* Debug output */
+		  apr_file_printf(fp_err, "Output:\n");
 
-		  return 0;
+		  /* Print out input buffer */
+		  apr_file_printf(fp_out, buffer);
+
+		  return 0; /* And I'm spent... */
 
 }
