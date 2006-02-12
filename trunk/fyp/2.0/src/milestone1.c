d9657c2b2a3202629b5057782343395b25ea684d
Conall O'Brien <conall@conall.net>
2006-02-12 04:20:22 +0000

    r2226@thog:  conall | 2006-02-12 04:18:03 +0000  #included linklist.h. Tried to use linklist data type as a  dynamic char* buffer for input. Compile errors experienced,  more work needed
     Shuffled a few comments around.
    
    
    git-svn-id: file:///home/conall/.svk/local@2283 df5d9cd5-2fe6-0310-9432-a9a2fbf40881
    
    committer: conall <conall@df5d9cd5-2fe6-0310-9432-a9a2fbf40881>

--- trunk/fyp/2.0/src/milestone1.c
+++ trunk/fyp/2.0/src/milestone1.c
@@ -11,33 +11,38 @@
 #include "apr.h"
 #include "apr_file_io.h"
 
-#include "dyn_page.h"
-
 /* Use String manipulation functions */
 #include <string.h>
 #include <stdlib.h>
 
 
+#include "linklist.h"
+
 int main(int argc, char *argv[]) {
 
 		  /* input buffer */
-		  char *buffer;  
-		  char *storage = "\0";  
+		  char *buffer, *eof;
+		  linklist * storage;
+		  item tmp;
 
 		  int i = 0; /* loop counter */
 
 		  /* APR pools, 1 for stdin, stdout & stderr */
 		  apr_pool_t *p, *q, *e; 
+
 		  /* APR files, for stdin, stdout & stderr */
 		  apr_file_t *fp_in, *fp_out, *fp_err; 
 
-		  apr_initialize(); /* Initialise APR */
-
+		  /* Initialise APR */
+		  apr_initialize(); 
 		  atexit(apr_terminate);
 
-		  apr_pool_create(&e, NULL); /* initialise APR pool e */
-		  apr_pool_create(&p, NULL); /* initialise APR pool p */
-		  apr_pool_create(&q, NULL); /* initialise APR pool q */
+		  storage = LinkListMkList();
+
+		  /* initialise APR pool e, p, q */
+		  apr_pool_create(&e, NULL);
+		  apr_pool_create(&p, NULL);
+		  apr_pool_create(&q, NULL);
 
 		  /* Open stdin, stdout & stderr */
 		  apr_file_open_stderr(&fp_err, e); 
@@ -48,16 +53,20 @@ int main(int argc, char *argv[]) {
 		  apr_file_printf(fp_err, "Input:\n"); 
 
 		  /* Grab 80 characters from stdin and store in buffer */
-		  while(!= EOF) {
+		  while(buffer[strlen(buffer)] != EOF) {
 					 apr_file_gets(buffer, 80, fp_in); 
-					 strcat(storage, buffer);
+					 /*strcat(storage, buffer);*/
+					 LinkListPush(storage, buffer);
 		  }
 
 		  /* Debug output */
 		  apr_file_printf(fp_err, "Output:\n");
 
-		  /* Print out input buffer */
-		  apr_file_printf(fp_out, storage);
+		  while(storage->head != NULL) {
+					 tmp = LinkListPop();
+					 /* Print out input buffer */
+					 apr_file_printf(fp_out, tmp->value);
+		  }
 
 		  exit(0); /* And I'm spent... */
 
