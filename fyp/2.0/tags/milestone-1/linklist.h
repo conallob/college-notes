01e8527dadb2a90f2e1b93e678def59a3042605c
Conall O'Brien <conall@conall.net>
2006-02-14 18:31:07 +0000

    Tagged as milestone-1
    git-svn-id: file:///home/conall/.svk/local@2428 df5d9cd5-2fe6-0310-9432-a9a2fbf40881
    
    committer: conall <conall@df5d9cd5-2fe6-0310-9432-a9a2fbf40881>

---
+++ fyp/2.0/tags/milestone-1/linklist.h
@@ -0,0 +1,41 @@
+/* 
+ * File: linklist.h 
+ * Description: doubley linked list - inspired by  
+ *		g_list.c from c-hey -- http://c-hey.redbrick.dcu.ie
+ * Author: Conall O'Brien <conallob@maths.tcd.ie>
+ * Date: $Date$
+ * 
+ * Id: $Id$
+ */
+
+#define STDIN_BUFFER_LEN 80
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef struct listnode {
+   void *val;
+   struct listnode *prev;
+   struct listnode *next;
+} item;
+
+
+typedef struct llist {
+	item *head;
+	item *tail;
+	int size;
+} linklist;
+
+
+linklist* LinkListMkList();
+
+int LinkListAddNode(linklist *list, void *value);
+
+int LinkListRmNode(linklist *list, item *node);
+
+int LinkListRmList(linklist *list);
+
+item* LinkListPop(linklist *list);
+
+int LinkListPush(linklist *list, void *val);
