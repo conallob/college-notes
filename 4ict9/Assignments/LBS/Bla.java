2849445871b413f6083198367cc3946a30c5f393
Conall O'Brien <conall@conall.net>
2006-02-16 21:24:24 +0000

    r2273@thog:  conall | 2006-02-16 20:51:09 +0000  * Bla.java: A basic Java app to read a file, and effeectively return it
     * cinemas: A CVS formatted dump of cinema listings for 4 cinemas
    
    
    git-svn-id: file:///home/conall/.svk/local@2438 df5d9cd5-2fe6-0310-9432-a9a2fbf40881
    
    committer: conall <conall@df5d9cd5-2fe6-0310-9432-a9a2fbf40881>

---
+++ 4ict9/Assignments/LBS/Bla.java
@@ -0,0 +1,35 @@
+import java.io.*; 
+ 
+class Bla { 
+    public static void main ( String args[] ) throws Exception { 
+        File data =  new File( "cinemas" ); 
+ 
+        if ( !data.exists() || !data.canRead() ) { 
+            System.out.println( "Ack! I'm a silly programming language which can't read " + data ); 
+            return; 
+        } 
+ 
+        if ( data.isDirectory() ) { 
+            String [] files = data.list(); 
+            for (int i=0; i< files.length; i++) 
+                System.out.println( files[i] ); 
+        } 
+        else 
+            try { 
+                FileReader fr = new FileReader ( data );
+                BufferedReader in = new BufferedReader( fr );
+                String line;
+                while ((line = in.readLine()) != null) {
+								if (line.endsWith(":")) {
+									System.out.println(line.replace(':',',')); 
+								} else {
+									if (line.length() > 2) {
+										System.out.println(line);
+									}
+								}	
+							}
+            } 
+            catch ( FileNotFoundException e ) {
+                System.out.println( "Ack! I'm a silly programming language who can't find your file!" ); 
+            }
+    } } 
